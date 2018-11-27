7954530756491da2787bd069557665ec7d5549c4
qianlan <qlhdtly12189@Gmail.com>
2018-11-27 21:36:38 +0800

    20181127bn

---
+++ 03/src/app.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">{{msg}}</div>
+</template>
+
+<script>
+    export default {
+        name: "app",
+        data(){
+            return {
+                msg: "1111111"
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    $h : 100px;
+    #app{
+        width: 1376px;
+        height: 774px;
+        background: url("./images/666.png") no-repeat center/cover;
+        color: red;
+        line-height: $h;
+    }
+</style>
---
+++ 03/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const {VueLoaderPlugin} = require("vue-loader");
+const HTMLPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+
+const isDev = process.env.NODE_ENV === "development";
+const config = {
+    mode: "development",
+    entry: path.join(__dirname, "src/index.js"),
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "dist")
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HTMLPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: "vue-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["vue-style-loader", "style-loader", "css-loader"]
+            },
+            {
+                test: /\.(jpg|jpeg|gif|svg|png)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            limit: 1024,
+                            name: "[name].[ext]"
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: ["vue-style-loader","css-loader","sass-loader"]
+            }
+        ]
+    }
+}
+
+if (isDev) {
+    config.devServer = {
+        port: "8080",
+        host: "localhost",
+        overlay: {
+            errors: true
+        },
+        open: true,
+        hotOnly: true
+    };
+    config.plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+
+module.exports = config;
