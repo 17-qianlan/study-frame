366c24fd0653bda558eb4ab699d76c52f7aba18a
qianlan <qlhdtly12189@Gmail.com>
2018-12-26 23:15:24 +0800

    20181226 axios和token

--- 07/client/axios.js
+++ 07/client/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import createStore from './store/store';
+import createRouter from './router/router';
 
 const state = createStore().state;
+const store = createStore();
+
+const router = createRouter();
 
 axios.defaults.timeout = 5000;
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
@@ -17,6 +21,7 @@ instance.interceptors.request.use(
         if (state.token) {
             config.headers.Authorization = `token${state.token}`;
         }
+        return config;
     },
     errors => {
         return Promise.reject(errors);
@@ -27,6 +32,19 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
     response => {
         return response;
+    },
+    errors => {
+        if (errors.response) {
+            switch (errors.response.status) {
+                case 401 :
+                    store.commit.userLogout();// 清除token和username以及以后会拓展的头像
+                    router.replace({
+                        path: '/control/login',
+                        query: { redirect: router.currentRoute.fullPath }
+                    });
+            };
+        }
+        return Promise.reject(errors.response);
     }
 );
 
--- 07/server/token/check-token.js
+++ 07/server/token/check-token.js
@@ -1,3 +1,6 @@
+// 这个token在任意需要验证的里边调用,以检测请求头里是否包含token,如果包含token, 用户下次向我们的应用请求受保护的资源的时候，可以在请求里带着我们给它签发的这个 Token，
+// 后端应用收到请求，检查签名，如果验证通过确定这个 Token 是我们自己签发的，那就可以为用户响应回他需要的资源。
+
 const jwt = require('jsonwebtoken');
 
 module.exports = async(ctx, next) => {
@@ -8,13 +11,20 @@ module.exports = async(ctx, next) => {
         };
     };
     let token = authorization.split('')[1];
+    let tokenContent;
     try {
-        await jwt.verify(token, 'sinner77');
+        tokenContent = await jwt.verify(token, 'sinner77');
     } catch (err) {
         ctx.status = 401;
         ctx.body = {
             saveTokenSuccess: false
         };
     }
-    await next();
+    if (tokenContent) {
+        await next();
+    } else {
+        ctx.body = {
+            verification: false
+        };
+    }
 };
